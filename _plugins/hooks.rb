0fc196355fd25ad6d36179d10008d9c4fa9f553c
Christine Tsou <christinet@qualtrics.com>
2019-12-11 22:06:06 +0900

    Add _posts to .gitignore

--- _plugins/hooks.rb
+++ _plugins/hooks.rb
@@ -1,15 +1,23 @@
 require 'fileutils'
 
-# Jekyll Hook - runs ruby script to generate _posts from
-# list of collections specified in the _config.yml
-Jekyll::Hooks.register :site, :after_reset do |site|
-  posts_dir = '_posts'
-  FileUtils.mkdir_p(posts_dir)
-  for collection in site.collections do
-  	label = collection[0]
-  	object = collection[1]
-  	for documents in object.docs do
-  	  FileUtils.cp(documents.relative_path, posts_dir)
-  	end
-  end
+posts_dir = '_posts'
+
+# Jekyll Hook - runs ruby script to generate _posts from list of
+# collections specified in the _config.yml before site generation
+Jekyll::Hooks.register :site, :after_init do |site|
+	FileUtils.rm_r(posts_dir)
+	FileUtils.mkdir_p(posts_dir)
+	for collection in site.config['collections'].keys do
+		dir_name = '_' + collection
+		if collection != 'posts' and Dir.exist?(dir_name)
+			collection_posts = Dir.entries(dir_name).reject{|entry| entry == '.' || entry == '..'}
+			copy_collection_into_posts_dir(dir_name, collection_posts, posts_dir)
+		end
+	end
+end
+
+def copy_collection_into_posts_dir(collection_dir, collection_posts, posts_dir)
+	for file in collection_posts do
+		FileUtils.cp(collection_dir + '/' + file, posts_dir)
+	end
 end
